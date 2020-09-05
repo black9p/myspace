bbbc7030f0076e3567e546bff2b536df88a4cd22
black9p <black9p@naver.com>
2020-09-05 23:08:13 +0900

    Set querydsl

---
+++ myspace-core/src/main/java/me/black9p/myspace/core/infra/config/QueryDslConfig.java
@@ -0,0 +1,24 @@
+package me.black9p.myspace.core.infra.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * JPAQueryFactory 설정
+ * @author black9p
+ */
+@Configuration
+public class QueryDslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
